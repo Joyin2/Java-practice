a447868b53fef3e124f14f342b1a71176143175e
Joyin2 <joyinlaskar2000@gmail.com>
2023-07-17 10:58:46 +0530

    first commit

---
+++ hello/hello.java
@@ -0,0 +1,10 @@
+package hello;
+import java.util.*;
+
+public class hello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int name = sc.nextInt();
+        System.out.println(name);
+    }
+}
---
+++ sum/sum.java
@@ -0,0 +1,13 @@
+package sum;
+import java.util.*;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c= sc.nextInt();
+        int sum = (a + b) / c;
+        System.out.println(sum);
+    }
+}
